6cfbbc7727d915bf78d397d7112203c6a194354e
phuccypher <phucphotpho2020@gmail.com>
2025-02-26 23:52:37 +0700

    bai tap java chuong 4

---
+++ CHUONG_4_JAVASCRIPT/BAI1A/1A.js
@@ -0,0 +1,7 @@
+var Ten, Tuoi;
+Ten = prompt("Bạn hãy nhập vào tên: ", "");
+Tuoi = prompt("Bạn hãy nhập vào tuổi: ", 20);
+
+document.write("<p>Chào bạn: <b>" + Ten + "</b></p>");
+document.write("<br>");
+document.write("<p>Tuổi của bạn là: <u>" + Tuoi + "</u></p>");
---
+++ CHUONG_4_JAVASCRIPT/BAI1B/1B.js
@@ -0,0 +1,9 @@
+var hoTen, Lop;
+
+Lop = prompt("Nhập lớp học: ", "");
+hoTen = prompt("Nhập họ và tên: ", "");
+
+document.write("<h1>TRƯỜNG ĐẠI HỌC BẠC LIÊU</h1>");
+document.write("<h2>KHOA CÔNG NGHỆ THÔNG TIN</h2><br>");
+document.write("<p class='Lop'>Lớp học: " + Lop + "</p>");
+document.write("<p class='hoTen'><i>Họ tên sinh viên: </i>" + hoTen + "</p>");
